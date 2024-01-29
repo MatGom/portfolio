ee8b4e6e1ff947a6e3b73f885619179c59b0ee78
Mateusz Gomolka <mdm.gomolka@gmail.com>
2024-01-29 20:00:39 +0000

    Add Home component

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import styles from './Home.module.css';
+import headerImage from '../images/header.svg';
+const Home = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <div className={styles.subtitles}>
+            <h2 className={styles.firstSubtitle}>Front-end</h2>
+            <h3 className={styles.secondSubtitle}>Web Developer</h3>
+          </div>
+          <div className={styles.image}>
+            <img src={headerImage} alt='Coding work station' />
+          </div>
+          <div className={styles.title}>
+            <h1>Mateusz Gomolka</h1>
+          </div>
+          <div className={styles.titleDecoration}>
+            <h1>Mateusz Gomolka</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Home;
