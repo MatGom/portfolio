6f303ed1647483e63625594cb6f6338c0df60364
Mateusz Gomolka <mdm.gomolka@gmail.com>
2024-02-01 00:57:24 +0000

    Add Footer component with styles and media queries

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import styles from './Footer.module.css';
+
+import { ReactComponent as Logo } from '../images/logo_white_lines.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopyright } from '@fortawesome/free-regular-svg-icons';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <p className={styles.description}>
+          <FontAwesomeIcon className={styles.copyright} icon={faCopyright} />
+          2024 Mateusz Gomolka, front-end web developer. All rights reserved.
+        </p>
+        <Logo className={styles.logo} />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
