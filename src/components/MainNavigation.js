bf6424eb78611631ddc00bdb74afe8a2bc81c39c
Mateusz Gomolka <mdm.gomolka@gmail.com>
2024-02-06 23:58:19 +0000

    Add media queries to the MainNavigation component and Add burger menu icon

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -3,17 +3,24 @@ import { ReactComponent as Logo } from '../images/logo_black_lines.svg';
 
 const MainNavigation = () => {
   return (
-    <nav className={styles.navigation}>
-      <ul className={styles.navList}>
-        <li className={styles.listItem}>Home</li>
-        <li className={styles.listItem}>About</li>
-        <li className={styles.listItem}>Projects</li>
-        <li className={styles.listItem}>Contact</li>
-        <li className={styles.listItem}>
-          <Logo className={styles.logo} />
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className={styles.navigation}>
+        <ul className={styles.navList}>
+          <li className={styles.listItem}>Home</li>
+          <li className={styles.listItem}>About</li>
+          <li className={styles.listItem}>Projects</li>
+          <li className={styles.listItem}>Contact</li>
+          <li className={`${styles.listItem} ${styles.logoContainer}`}>
+            <Logo className={styles.logo} />
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.burger}>
+        <div className={`${styles.burgerBar} ${styles.burgerBarOne}`}></div>
+        <div className={`${styles.burgerBar} ${styles.burgerBarTwo}`}></div>
+        <div className={`${styles.burgerBar} ${styles.burgerBarThree}`}></div>
+      </div>
+    </>
   );
 };
 
