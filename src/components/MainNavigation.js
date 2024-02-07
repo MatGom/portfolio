eac40619dec8ff6ea1630e2fb73923b94a7eed94
Mateusz Gomolka <mdm.gomolka@gmail.com>
2024-02-07 16:46:44 +0000

    Add styles to burger menu icon and add functionality to toggle burger menu

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,10 +1,32 @@
 import styles from './MainNavigation.module.css';
 import { ReactComponent as Logo } from '../images/logo_black_lines.svg';
 
+import { useState } from 'react';
+
 const MainNavigation = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  let navClasses;
+  let burgerClasses;
+  let burgerNavigationClasses;
+
+  if (!menuIsOpen) {
+    navClasses = `${styles.navigation} ${styles.navigationClosed}`;
+    burgerClasses = `${styles.burger}`;
+    burgerNavigationClasses = `${styles.burgerNavigation} ${styles.burgerNavigationClosed}`;
+  } else if (menuIsOpen) {
+    navClasses = `${styles.navigation}`;
+    burgerClasses = `${styles.burger} ${styles.menuOpen}`;
+    burgerNavigationClasses = `${styles.burgerNavigation}`;
+  }
+
+  const toggleMenuHandler = () => {
+    setMenuIsOpen(!menuIsOpen);
+  };
+
   return (
     <>
-      <nav className={styles.navigation}>
+      <nav className={navClasses}>
         <ul className={styles.navList}>
           <li className={styles.listItem}>Home</li>
           <li className={styles.listItem}>About</li>
@@ -15,10 +37,19 @@ const MainNavigation = () => {
           </li>
         </ul>
       </nav>
-      <div className={styles.burger}>
-        <div className={`${styles.burgerBar} ${styles.burgerBarOne}`}></div>
-        <div className={`${styles.burgerBar} ${styles.burgerBarTwo}`}></div>
-        <div className={`${styles.burgerBar} ${styles.burgerBarThree}`}></div>
+      <div className={burgerNavigationClasses}>
+        {menuIsOpen ? null : (
+          <>
+            <Logo className={styles.logo} />
+            <p>Mateusz Gomolka</p>
+            <p>Front-end web developer</p>
+          </>
+        )}
+        <div onClick={toggleMenuHandler} className={burgerClasses}>
+          <div className={`${styles.burgerBar} ${styles.burgerBarOne}`}></div>
+          <div className={`${styles.burgerBar} ${styles.burgerBarTwo}`}></div>
+          <div className={`${styles.burgerBar} ${styles.burgerBarThree}`}></div>
+        </div>
       </div>
     </>
   );
