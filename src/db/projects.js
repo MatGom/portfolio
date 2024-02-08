81ddece2124f4493b740182f1e774af737568a30
Mateusz Gomolka <mdm.gomolka@gmail.com>
2024-02-08 19:18:20 +0000

    Adjust styles and media queries across the website

--- src/db/projects.js
+++ src/db/projects.js
@@ -2,10 +2,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
+import projectOneImage from '../images/project.png';
+
 export const projects = [
   {
     id: 'project1',
-    image: '',
+    image: projectOneImage,
     imageDescription: '',
     title: 'Title1',
     description:
@@ -21,42 +23,36 @@ export const projects = [
     id: 'project2',
     image: '',
     imageDescription: '',
-    title: 'Title2',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id porttitor leo. Donec placerat nec quam vitae congue. Quisque congue magna lorem, non consequat magna porttitor non. Curabitur faucibus lacinia magna id scelerisque. Nam ante ligula, consequat semper aliquet eget, pharetra vel nisi. Donec auctor imperdiet commodo. Aenean sollicitudin tortor metus.',
+    title: 'TBA',
+    description: 'Coming soon...',
     linkIcon: <FontAwesomeIcon icon={faGithub} />,
     arrowIcon: <FontAwesomeIcon icon={faChevronRight} />,
-    backDescription:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id porttitor leo. Donec placerat nec quam vitae congue. Quisque congue magna lorem, non consequat magna porttitor non. Curabitur faucibus lacinia magna id scelerisque. Nam ante ligula, consequat semper aliquet eget, pharetra vel nisi. Donec auctor imperdiet commodo. Aenean sollicitudin tortor metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id porttitor leo. Donec placerat nec quam vitae congue. Quisque congue magna lorem, non consequat magna porttitor non. Curabitur faucibus lacinia magna id scelerisque. Nam ante ligula, consequat semper aliquet eget, pharetra vel nisi. Donec auctor imperdiet commodo. Aenean sollicitudin tortor metus.',
-    link: 'http://google.com',
-    githubLink: 'http://github.com',
+    backDescription: 'Coming soon...',
+    link: '',
+    githubLink: '',
   },
   {
     id: 'project3',
     image: '',
     imageDescription: '',
-    title: 'Title3',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id porttitor leo. Donec placerat nec quam vitae congue. Quisque congue magna lorem, non consequat magna porttitor non. Curabitur faucibus lacinia magna id scelerisque. Nam ante ligula, consequat semper aliquet eget, pharetra vel nisi. Donec auctor imperdiet commodo. Aenean sollicitudin tortor metus.',
+    title: 'TBA',
+    description: 'Coming soon...',
     linkIcon: <FontAwesomeIcon icon={faGithub} />,
     arrowIcon: <FontAwesomeIcon icon={faChevronRight} />,
-    backDescription:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id porttitor leo. Donec placerat nec quam vitae congue. Quisque congue magna lorem, non consequat magna porttitor non. Curabitur faucibus lacinia magna id scelerisque. Nam ante ligula, consequat semper aliquet eget, pharetra vel nisi. Donec auctor imperdiet commodo. Aenean sollicitudin tortor metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id porttitor leo. Donec placerat nec quam vitae congue. Quisque congue magna lorem, non consequat magna porttitor non. Curabitur faucibus lacinia magna id scelerisque. Nam ante ligula, consequat semper aliquet eget, pharetra vel nisi. Donec auctor imperdiet commodo. Aenean sollicitudin tortor metus.',
-    link: 'http://google.com',
-    githubLink: 'http://github.com',
+    backDescription: 'Coming soon...',
+    link: '',
+    githubLink: '',
   },
   {
     id: 'project4',
     image: '',
     imageDescription: '',
     title: 'Title4',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id porttitor leo. Donec placerat nec quam vitae congue. Quisque congue magna lorem, non consequat magna porttitor non. Curabitur faucibus lacinia magna id scelerisque. Nam ante ligula, consequat semper aliquet eget, pharetra vel nisi. Donec auctor imperdiet commodo. Aenean sollicitudin tortor metus.',
+    description: 'Coming soon...',
     linkIcon: <FontAwesomeIcon icon={faGithub} />,
     arrowIcon: <FontAwesomeIcon icon={faChevronRight} />,
-    backDescription:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id porttitor leo. Donec placerat nec quam vitae congue. Quisque congue magna lorem, non consequat magna porttitor non. Curabitur faucibus lacinia magna id scelerisque. Nam ante ligula, consequat semper aliquet eget, pharetra vel nisi. Donec auctor imperdiet commodo. Aenean sollicitudin tortor metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id porttitor leo. Donec placerat nec quam vitae congue. Quisque congue magna lorem, non consequat magna porttitor non. Curabitur faucibus lacinia magna id scelerisque. Nam ante ligula, consequat semper aliquet eget, pharetra vel nisi. Donec auctor imperdiet commodo. Aenean sollicitudin tortor metus.',
-    link: 'http://google.com',
-    githubLink: 'http://github.com',
+    backDescription: 'Coming soon...',
+    link: '',
+    githubLink: '',
   },
 ];
