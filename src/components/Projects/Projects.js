d625425795112cf095031e2c3033883e54f73c2b
Mateusz Gomolka <mdm.gomolka@gmail.com>
2024-08-07 19:42:46 +0100

    Add animation effect to projectsWrapper

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,13 +1,40 @@
+import React, { useEffect, useRef } from 'react';
 import styles from './Projects.module.css';
 
 import Project from './Project/Project';
 import { projects } from '../../db/projects';
 
 const Projects = function () {
+  const projectsWrapperRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add(styles.animate);
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      { threshold: 0.1 }
+    );
+
+    if (projectsWrapperRef.current) {
+      observer.observe(projectsWrapperRef.current);
+    }
+
+    return () => {
+      if (projectsWrapperRef.current) {
+        observer.unobserve(projectsWrapperRef.current);
+      }
+    };
+  }, []);
+
   return (
     <section id='projects' className={styles.projects}>
       <h3 className='section-title'>Projects</h3>
-      <div className={styles.projectsWrapper}>
+      <div ref={projectsWrapperRef} className={styles.projectsWrapper}>
         {projects.map(project => (
           <Project
             key={project.id}
